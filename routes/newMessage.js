6499bfc626e11613ff8290fb26e0c35582cba45e
cosmic <yafetyohaness@gmail.com>
2023-02-02 11:13:57 -0500

    Make POST route

--- routes/newMessage.js
+++ routes/newMessage.js
@@ -3,4 +3,13 @@ const route = express.Router();
 route.get("/", (req, res) => {
   res.render("form");
 });
+route.post("/", (req, res) => {
+  const message = {
+    text: req.body.message,
+    user: req.body.userName,
+    added: new Date(),
+  };
+  console.log(message);
+  res.redirect("/");
+});
 module.exports = route;
